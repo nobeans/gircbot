15e6c0deeb1529f7cadf94e062217e4ea7490f52
nobeans <nobeans@gmail.com>
2010-03-03 00:45:05 +0900

    fix for message format

--- LogBot.groovy
+++ LogBot.groovy
@@ -5,12 +5,12 @@ class LogBot extends PircBot {
 
     private appender
 
-    public void onJoin(String channel, String sender, String login, String hostname) {
-        append("JOIN", channel, sender, "* $sender (${login}@${hostname}) has joined $channel")
+    void onJoin(String channel, String sender, String login, String hostname) {
+        append("JOIN", channel, sender, "+ ${sender} (${login}@${hostname})")
         op(channel, sender) // auto op provider
     }
 
-    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+    void onMessage(String channel, String sender, String login, String hostname, String message) {
         append("PRIVMSG", channel, sender, message)
 
         // なると消失状態に対応するため、"!bye"発言に反応してPARTするようにしている。
@@ -19,30 +19,56 @@ class LogBot extends PircBot {
         }
     }
 
-    public void onNotice(String sourceNick, String sourceLogin, String sourceHostname, String target, String notice) {
+    void onNotice(String sourceNick, String sourceLogin, String sourceHostname, String target, String notice) {
         append("NOTICE", target, sourceNick, notice)
     }
 
-    public void onMode(String channel, String sourceNick, String sourceLogin, String sourceHostname, String mode) {
-        append("MODE", channel, sourceNick, "* " + sourceNick + " sets mode " + mode)
+    void onMode(String channel, String sourceNick, String sourceLogin, String sourceHostname, String mode) {
+        append("MODE", channel, sourceNick, "* ${sourceNick} changed mode (${mode})")
     }
 
-    public void onPart(String channel, String sender, String login, String hostname) {
-        append("PART", channel, sender, "* " + sender + " (" + login + "@" + hostname + ") has left " + channel)
+    void onPart(String channel, String sender, String login, String hostname) {
+        append("PART", channel, sender, "- ${sender} (PART) (${login}@${hostname})")
     }
 
-    public void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
-        if (changed) append("TOPIC", channel, setBy, topic)
+    void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
+        if (changed) append("TOPIC", channel, setBy, "* TOPIC: ${topic} (by ${setBy})")
     }
 
-    public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
-        append("TOPIC", channel, kickerNick, "* " + recipientNick + " was kicked from " + channel + " by " + kickerNick)
+    void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+        append("TOPIC", channel, kickerNick, "- ${recipientNick} was kicked by ${kickerNick} (${reason})")
     }
 
     // INVITEされればどこへでも。ただし入室キーが設定されてるチャンネルは無理。
-    public void onInvite(String targetNick, String sourceNick, String sourceLogin, String sourceHostname, String channel) {
+    void onInvite(String targetNick, String sourceNick, String sourceLogin, String sourceHostname, String channel) {
         joinChannel(channel)
         sendNotice(channel, "一生懸命ロギングするのでよろしくお願いします!")
+    }
+/*
+      'NICK'    => '* {nick} -> {newnick}',
+      'QUIT'    => '- {nick} (QUIT: {msg}) ({prefix:user}@{prefix:host})',
+*/
+    void onNickChange(String oldNick, String login, String hostname, String newNick) {
+        getChannelsJoinedUser(newNick).each { channel ->
+            append("NICK", channel, oldNick, "* ${oldNick} -> ${newNick}")
+        }
+    }
+
+    void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
+        getChannelsJoinedUser(sourceNick).each { channel ->
+            append("QUI", channel, oldNick, "- ${sourceNick} (QUIT: ${reason}) (${sourceLogin}@${sourceHostname})")
+        }
+    }
+
+    private getChannelsJoinedUser(nick) {
+        channels.findAll { channel ->
+            nick in getUsers(channel).nick
+        }
+    }
+
+    void handleLine(String line) {
+        println ">>>>>>>" + line
+        super.handleLine(line)
     }
 
     public void onDisconnect() {
