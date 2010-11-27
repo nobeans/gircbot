27de91d499c175d7549dea9e3723369ecd0b52ab
Yasuharu NAKANO <ynak@jggug.org>
2010-11-28 04:17:43 +0900

    fixed to manage a primary status for each channel

--- src/main/java/org/jggug/kobo/gircbot/core/PrimaryMonitor.java
+++ src/main/java/org/jggug/kobo/gircbot/core/PrimaryMonitor.java
@@ -1,7 +1,9 @@
 package org.jggug.kobo.gircbot.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jibble.pircbot.User;
@@ -13,7 +15,7 @@ public class PrimaryMonitor {
     protected String defaultChannel;
     protected List<String> orderedPrimaryNicks;
     protected IrcControl ircControl;
-    protected boolean wasPrimary = false;
+    protected Map<String, Boolean> primaryStatus = new HashMap<String, Boolean>();
 
     public PrimaryMonitor(String defaultChannel, List<String> orderedPrimaryNicks, IrcControl ircControl) {
         this.defaultChannel = defaultChannel;
@@ -43,18 +45,19 @@ public class PrimaryMonitor {
         // when the first element is the own nick, this bot is primary.
         int index = joinedPrimaryNicks.indexOf(myNick);
         boolean isPrimary = (index == 0);
+        boolean wasPrimary = (primaryStatus.containsKey(channel) ? primaryStatus.get(channel) : false);
         if (LOG.isDebugEnabled()) LOG.debug(String.format("checking: %s in %s %s (primary:%s)", myNick, channel, joinedPrimaryNicks, isPrimary));
         if (isPrimary && !wasPrimary) {
             String message = MessageUtils.getMessage("core.GircBot.isPrimary.active");
             ircControl.sendMessage(channel, message);
             LOG.info(String.format("%s: %s in %s %s", message, myNick, channel, joinedPrimaryNicks));
-            wasPrimary = true;
+            primaryStatus.put(channel, true);
         }
         else if (!isPrimary && wasPrimary) {
             String message = MessageUtils.getMessage("core.GircBot.isPrimary.standby");
             ircControl.sendMessage(channel, message);
             LOG.info(String.format("%s: %s in %s %s", message, myNick, channel, joinedPrimaryNicks));
-            wasPrimary = false;
+            primaryStatus.put(channel, false);
         }
         else {
             assert (isPrimary == wasPrimary) : "Status is not changed";
