a541eff4464adbe59caf5611d90555c1a0357fe7
Yasuharu Nakano <ynak@jggug.org>
2014-11-21 00:20:02 +0900

    Update sample scripts

--- sample/configbot.groovy
+++ sample/configbot.groovy
@@ -1,20 +1,25 @@
 @GrabResolver(name="bintray", root="http://dl.bintray.com/nobeans/maven")
 @Grab("org.jggug.kobo:gircbot:0.2")
 import org.jggug.kobo.gircbot.builder.GircBotBuilder
+
+// You can write this in a configuration file
+def config = new ConfigSlurper().parse '''
 import org.jggug.kobo.gircbot.core.Reactor
 import org.jggug.kobo.gircbot.reactors.Dictionary
 import org.jggug.kobo.gircbot.reactors.OpDistributor
 import org.jggug.kobo.gircbot.reactors.InviteAndByeResponder
 import org.jggug.kobo.gircbot.jobs.Reminder
 
-new GircBotBuilder().config {
+gircbot {
     server {
-        host "localhost"
-        port 6667
+        host = "localhost"
+        port = 6667
     }
-    nick "gircbot"
-    channel { autoJoinTo "#test", "#lounge" }
-    reactors(
+    nick = "gircbot"
+    channel {
+        autoJoinTo = ["#test", "#lounge"]
+    }
+    reactors = [
         new Dictionary(new File(System.properties['user.home'], ".gircbot-dictionary")),
         new OpDistributor(),
         new InviteAndByeResponder(),
@@ -23,9 +28,12 @@ new GircBotBuilder().config {
                 println "$channel $message"
             }
         },
-    )
-    jobs(
+    ]
+    jobs = [
         new Reminder(new File(System.properties['user.home'], ".gircbot-reminder")),
-    )
-}.start()
+    ]
+}
+'''
+
+new GircBotBuilder(config: config.gircbot.flatten()).start()
 
