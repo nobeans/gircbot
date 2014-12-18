f42e11276934c1b58e7b74904badf01354c53a1e
Yasuharu Nakano <ynak@jggug.org>
2014-12-18 15:13:46 +0900

    Improve durability of TimeMonitor

--- src/main/java/org/jggug/kobo/gircbot/core/TimeMonitor.java
+++ src/main/java/org/jggug/kobo/gircbot/core/TimeMonitor.java
@@ -1,5 +1,8 @@
 package org.jggug.kobo.gircbot.core;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,6 +11,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TimeMonitor {
+
+    private static final Log LOG = LogFactory.getLog(TimeMonitor.class);
 
     private static final long START_DELAY = 0L; // sec
     private static final long INTERVAL = 1L; // sec
@@ -24,19 +29,23 @@ public class TimeMonitor {
         if (started) {
             throw new IllegalStateException("Time monitor is already started.");
         }
-        Runnable runnable = createRunnableWithCopiedListeners();
+        Runnable runnable = createWorkerRunnable();
         executor.scheduleAtFixedRate(runnable, START_DELAY, INTERVAL, TimeUnit.SECONDS);
         started = true;
     }
 
-    private Runnable createRunnableWithCopiedListeners() {
+    private Runnable createWorkerRunnable() {
         final List<TimeEventListener> localListeners = new ArrayList<TimeEventListener>(listeners);
         return new Runnable() {
             @Override
             public void run() {
                 Date time = new Date();
                 for (TimeEventListener listener : localListeners) {
-                    listener.invoke(time);
+                    try {
+                        listener.invoke(time);
+                    } catch (Exception e) {
+                        LOG.warn("Failed to invoke TimeEventListener: " + listener, e);
+                    }
                 }
             }
         };
