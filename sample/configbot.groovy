4347fe3b4400dc8ca7a7c9a26fe30f4ddb8aa04e
Yasuharu Nakano <ynak@jggug.org>
2014-12-18 14:51:36 +0900

    Tweak samples

--- sample/configbot.groovy
+++ sample/configbot.groovy
@@ -1,14 +1,14 @@
 @GrabResolver(name="bintray", root="http://dl.bintray.com/nobeans/maven")
-@Grab("org.jggug.kobo:gircbot:0.2")
+@Grab("org.jggug.kobo:gircbot:0.4")
 import org.jggug.kobo.gircbot.builder.GircBotBuilder
 
 // You can write this in a configuration file
 def config = new ConfigSlurper().parse '''
 import org.jggug.kobo.gircbot.core.Reactor
-import org.jggug.kobo.gircbot.reactors.Dictionary
-import org.jggug.kobo.gircbot.reactors.OpDistributor
-import org.jggug.kobo.gircbot.reactors.InviteAndByeResponder
 import org.jggug.kobo.gircbot.jobs.Reminder
+import org.jggug.kobo.gircbot.reactors.Dictionary
+import org.jggug.kobo.gircbot.reactors.InviteAndByeResponder
+import org.jggug.kobo.gircbot.reactors.OpDistributor
 
 gircbot {
     server {
@@ -20,7 +20,7 @@ gircbot {
         autoJoinTo = ["#test", "#lounge"]
     }
     reactors = [
-        new Dictionary(new File(System.properties['user.home'], ".gircbot-dictionary")),
+        new Dictionary(new File(System.properties['user.home'], ".gircbot/dictionary")),
         new OpDistributor(),
         new InviteAndByeResponder(),
         new Reactor() {
@@ -30,7 +30,7 @@ gircbot {
         },
     ]
     jobs = [
-        new Reminder(new File(System.properties['user.home'], ".gircbot-reminder")),
+        new Reminder(new File(System.properties['user.home'], ".gircbot/reminder")),
     ]
 }
 '''
--- sample/samplebot.groovy
+++ sample/samplebot.groovy
@@ -1,11 +1,11 @@
 @GrabResolver(name="bintray", root="http://dl.bintray.com/nobeans/maven")
-@Grab("org.jggug.kobo:gircbot:0.2")
+@Grab("org.jggug.kobo:gircbot:0.4")
 import org.jggug.kobo.gircbot.builder.GircBotBuilder
 import org.jggug.kobo.gircbot.core.Reactor
-import org.jggug.kobo.gircbot.reactors.Dictionary
-import org.jggug.kobo.gircbot.reactors.OpDistributor
-import org.jggug.kobo.gircbot.reactors.InviteAndByeResponder
 import org.jggug.kobo.gircbot.jobs.Reminder
+import org.jggug.kobo.gircbot.reactors.Dictionary
+import org.jggug.kobo.gircbot.reactors.InviteAndByeResponder
+import org.jggug.kobo.gircbot.reactors.OpDistributor
 
 new GircBotBuilder().config {
     server {
@@ -15,7 +15,7 @@ new GircBotBuilder().config {
     nick "gircbot"
     channel { autoJoinTo "#test", "#lounge" }
     reactors(
-        new Dictionary(new File(System.properties['user.home'], ".gircbot-dictionary")),
+        new Dictionary(new File(System.properties['user.home'], ".gircbot/dictionary")),
         new OpDistributor(),
         new InviteAndByeResponder(),
         new Reactor() {
@@ -25,7 +25,7 @@ new GircBotBuilder().config {
         },
     )
     jobs(
-        new Reminder(new File(System.properties['user.home'], ".gircbot-reminder")),
+        new Reminder(new File(System.properties['user.home'], ".gircbot/reminder")),
     )
 }.start()
 
